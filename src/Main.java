8eaaa9f3c3dd15062e67fd3c2562bf7dc5714bf7
joycesherif <joycesh2020@gmail.com>
2026-05-09 05:39:51 +0300

    fixed guest dashboard

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.DatabaseConnection;
 import model.HotelDataBase;
 
 public class Main extends Application {
@@ -33,7 +34,6 @@ public class Main extends Application {
     public static void main(String[] args) {
         // Initialize database once
         HotelDataBase.getInstance().populateDummyData();
-        System.out.println("DB location: " + new java.io.File("hotel.db").getAbsolutePath());
 
         // Launch JavaFX
         launch(args);
